2147b77515de341c904c9cc90e42e2374cf607b2
zhujiaojiao <zhujiaojiao@dm-ai.com>
2021-08-24 08:42:35 +0800

    setup的用法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,34 @@
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
   <button type="button" @click="state.count++">
     count is: {{ state.count }}
   </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="home">
+    这里是一个计数器 <span class="red">{{ count }}</span> <br />
+    <button @click="countAdd">{{ btnText }}</button>
+  </div>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0 });
+import { ref } from "vue";
+// 导出依然是个对象，不过对象中只有一个 setup 函数
+
+// 定义一个不需要改变的数据
+const btnText = "点这个按钮上面的数字会变";
+// 定义一个 count 的响应式数据，并赋值为 0
+const count = ref(0);
+// 定义一个函数，修改 count 的值。
+const countAdd = () => {
+  count.value++;
+};
 </script>
 
 <style scoped>
